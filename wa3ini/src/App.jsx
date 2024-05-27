2cf018666d4c5e002f679e208b43dd04180e0a13
Ahmed <ahmedmasmoudiofficial@gmail.com>
2024-05-27 11:29:14 +0100

    .

--- wa3ini/src/App.jsx
+++ wa3ini/src/App.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import Sidebar from './components/SideBar.jsx';
-import MainContent from './components/MainContent.jsx';
-
-export default function App() {
-
-  return (
-    // <section className="grid grid-cols-12 h-screen relative text-red-500">
-    <section className="h-screen">
-      <Sidebar />
-      <MainContent />
-    </section>
-  )
-}
-=======
 import Sidebar from "./components/SideBar.jsx";
 import MainContent from "./components/MainContent.jsx";
  function App(){
@@ -22,4 +7,3 @@ import MainContent from "./components/MainContent.jsx";
   
 }
 export default App;
->>>>>>> 57a7284660d2672f88cdc8576852989d6c33828a
--- wa3ini/src/components/MainContent.jsx
+++ wa3ini/src/components/MainContent.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-
-=======
 import Header from "./Header.jsx";
 import CardView from "./CardView.jsx";
->>>>>>> 57a7284660d2672f88cdc8576852989d6c33828a
 function Card() {
     return (
       <div className="bg-pink-500 lg:col-span-3 md:col-span-4 sm:col-span-6 col-span-12"></div>
@@ -13,23 +9,8 @@ function Card() {
 export default function MainContent() {
     return <>
       <div className=" bg-blue-500 sm:ml-64">
-<<<<<<< HEAD
-        <div className="h-64 bg-yellow-500"></div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-          <div className="bg-pink-500 h-48"><button>autism</button></div>
-          <div className="bg-pink-500 h-48"><button>no hear</button></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-        </div>
-=======
         <div className="h-64"> <Header /></div>
         <CardView />
->>>>>>> 57a7284660d2672f88cdc8576852989d6c33828a
       </div>
     </>
   }
